3a607e987966e857c01e88bdfcdfb8711d825772
MrOutis <108561034+MrOutis@users.noreply.github.com>
2023-12-13 01:47:03 -0500

    Update configuração.js

--- configuração.js
+++ configuração.js
@@ -4,4 +4,4 @@ const chalk = require("chalk")
 //Caso for mudar mude dentro das aspas "assim" tome cuidado para não tirar as aspas caso tire coloque de novo pois vai da erro
 
 //[FOTO DO MENU]\\
-global.menu = "https://telegra.ph/file/9ea4b58e31e9f50e2cf77.jpg"
+global.menu = "https://es.web.img3.acsta.net/r_1920_1080/pictures/14/10/27/10/59/140682.jpg"
